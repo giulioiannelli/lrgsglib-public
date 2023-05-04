e14a24634717e6a9adebe7ca6c30ec8636eeb8c6
giulioiannelli <giulioiannelli.w@gmail.com>
2023-05-04 09:46:24 +0200

    upd

--- src/LRGSG_package/LRGSG.py
+++ src/LRGSG_package/LRGSG.py
@@ -13,13 +13,15 @@ from farrow_and_ball import *
 from numpy.linalg import eigvals
 from scipy.cluster import hierarchy
 from scipy.cluster.hierarchy import fcluster, dendrogram, linkage
-from scipy.linalg import expm, fractional_matrix_power
+from scipy.linalg import expm, fractional_matrix_power, ishermitian
+from scipy.sparse.linalg import eigs, eigsh, ArpackNoConvergence
 from scipy.spatial.distance import squareform
 #
 from tqdm import tqdm
 #
 ePDF = ".pdf"
 #
+datPath_lmin = "data/lmin/"
 pltPath_Sm1C = "plot/Sm1_and_C/"
 #
 def smallest_prob_for_erconn(N, pstart=0.1, halving=0.8, testset=100):
@@ -38,6 +40,13 @@ def logpsace_prob_erconn(N, pHigh=0.5, pstart=0.1, halving=0.8, testset=100):
     lowest_prob = smallest_prob_for_erconn(N, pstart, halving, testset)
     lsp = np.logspace(np.log10(lowest_prob), np.log10(pHigh), num=10)
     return lsp
+def get_graph_lapl(G, is_signed=True):
+    if is_signed:
+        lapl = slaplacian_matrix(G).asfptype()
+    else:
+        lapl = nx.laplacian_matrix(G).asfptype()
+    return lapl
+
 def get_graph_lspectrum(G, is_signed=False):
     if is_signed:
         A = nx.adjacency_matrix(G).toarray()
@@ -49,6 +58,65 @@ def get_graph_lspectrum(G, is_signed=False):
         w = nx.laplacian_spectrum(G)
     return L, w
 
+def slaplacian_matrix(G, nodelist=None, weight="weight"):
+    """Returns the signed Laplacian matrix of G.
+
+    The graph Laplacian is the matrix L = D - A, where
+    A is the adjacency matrix and D is the diagonal matrix of node degrees.
+
+    Parameters
+    ----------
+    G : graph
+       A NetworkX graph
+
+    nodelist : list, optional
+       The rows and columns are ordered according to the nodes in nodelist.
+       If nodelist is None, then the ordering is produced by G.nodes().
+
+    weight : string or None, optional (default='weight')
+       The edge data key used to compute each value in the matrix.
+       If None, then each edge has weight 1.
+
+    Returns
+    -------
+    L : SciPy sparse array
+      The Laplacian matrix of G.
+
+    Notes
+    -----
+    For MultiGraph, the edges weights are summed.
+
+    See Also
+    --------
+    to_numpy_array
+    normalized_laplacian_matrix
+    laplacian_spectrum
+
+    Examples
+    --------
+    For graphs with multiple connected components, L is permutation-similar
+    to a block diagonal matrix where each block is the respective Laplacian
+    matrix for each component.
+
+    >>> G = nx.Graph([(1, 2), (2, 3), (4, 5)])
+    >>> print(nx.laplacian_matrix(G).toarray())
+    [[ 1 -1  0  0  0]
+     [-1  2 -1  0  0]
+     [ 0 -1  1  0  0]
+     [ 0  0  0  1 -1]
+     [ 0  0  0 -1  1]]
+
+    """
+    import scipy as sp
+    import scipy.sparse  # call as sp.sparse
+
+    if nodelist is None:
+        nodelist = list(G)
+    A = nx.to_scipy_sparse_array(G, nodelist=nodelist, weight=weight, format="csr")
+    n, m = A.shape
+    D = sp.sparse.csr_array(sp.sparse.spdiags(np.abs(A.sum(axis=1)), 0, m, n, format="csr"))
+    return D - A
+
 def get_graph_lspectrum_rw(G, is_signed=False):
     A = nx.adjacency_matrix(G).toarray()
     D = np.diag(np.abs(A).sum(axis=1))
@@ -59,14 +127,12 @@ def get_graph_lspectrum_rw(G, is_signed=False):
         w = nx.laplacian_spectrum(G)
     return L, w
 
-def entropy(G, steps=600, is_signed=False, wTresh=1e-15):
+def entropy(G, steps=600, is_signed=False, wTresh=1e-15, t1=-2, t2=5):
     N = G.number_of_nodes()
 
     L, w = get_graph_lspectrum_rw(G, is_signed=is_signed)
     wSig = w[w>wTresh]
     
-    t1 = -2#np.log10(1. / np.max(wSig))
-    t2 = 5#np.log10(10. / np.min(wSig))
     t = np.logspace(t1,t2, int(steps))
     S = np.zeros(len(t))
     VarL = np.zeros(len(t))
