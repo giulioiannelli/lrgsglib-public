1911bfa47ad9ddbb2d94ef9f0b99be5a09a31f49
giulioiannelli <giulioiannelli.w@gmail.com>
2023-11-28 15:07:57 +0100

    cluster launch pt2

--- src/script_lanzar.py
+++ src/script_lanzar.py
@@ -3,17 +3,17 @@ import os
 import sys
 
 L = int(sys.argv[1])
-p_l = np.linspace(0.05, 0.15, 10)#float(sys.argv[2])
+p_l = np.linspace(0.075, 0.15, 10)#float(sys.argv[2])
 T_l = np.linspace(0.005, 0.5, 100)
 
 for p in p_l:
     for T in T_l:
-        # os.system(
-        #     "slanzarv python3.10 lattc2dsq_IsingDynamics_mClusters.py "
-        #     + f"{L} {p:.3g} {T:.4g} -nA 1000"
-        # )
-        print(
-            "slanzarv python3.10 lattc2dsq_IsingDynamics_mClusters.py "
+        os.system(
+            "slanzarv python lattc2dsq_IsingDynamics_mClusters.py "
             + f"{L} {p:.3g} {T:.4g} -nA 1000"
         )
+        # print(
+        #     "slanzarv python lattc2dsq_IsingDynamics_mClusters.py "
+        #     + f"{L} {p:.3g} {T:.4g} -nA 1000"
+        # )
 
