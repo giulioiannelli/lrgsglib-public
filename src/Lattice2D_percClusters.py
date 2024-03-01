f2d4a6400a3aa87a8718f5917df89f427a004975
Giulio Iannelli <giulioiannelli.w@gmail.com>
2024-03-01 15:39:48 +0100

    slanzarv ready

---
+++ src/Lattice2D_percClusters.py
@@ -0,0 +1,29 @@
+from Lattice2D_percClusters_Parser import *
+#
+args = parser.parse_args()
+#
+merged_dict = Counter()
+#
+lattice = Lattice2D(args.L, pflip=args.p, geometry=args.geometry)
+def file_path_maker(mpath = lattice.lrgsgpath, ppath= args.p, 
+                    napath = args.number_of_averages, 
+                    spath = args.out_suffix):
+    return f'{mpath}p={ppath:.3g}_na={napath}_{spath}.pkl'
+#
+filename = file_path_maker()
+if os.path.exists(filename):
+    os.exit(0)
+#
+for avg in range(args.number_of_averages):
+    lattice = Lattice2D(args.L, pflip=args.p, geometry=args.geometry)
+    lattice.flip_random_fract_edges()
+    dist_dict = lattice.cluster_distribution_list()
+    merged_dict += Counter(dist_dict)
+    try:
+        filenameold = file_path_maker(napath=avg)
+        os.remove(filenameold)
+    except OSError:
+        pass
+    filename = file_path_maker(napath=avg+1)
+    with open(filename, 'wb') as file:
+        pickle.dump(merged_dict, file)
---
+++ src/Lattice2D_percClusters_Serialiser.py
@@ -0,0 +1,45 @@
+import os
+import sys
+#
+programName = "Lattice2D_percClusters"
+List = [8, 16, 32, 64, 128, 256, 512]
+plist = [0.01, 0.034, 0.08, 0.103, 0.12, 0.206, 0.412]
+
+if "--print" in sys.argv:
+    do_print = True
+slanzarv_OPT = False
+if "--slanzarv" in sys.argv:
+    slanzarv_OPT = True
+    try:
+        index = sys.argv.index("--slanzarv")
+        slanzarv_MEMMB = sys.argv[index+1]
+    except IndexError:
+        slanzarv_MEMMB = 1024
+if "-n" in sys.argv:
+    index = sys.argv.index("-n")
+    noAvg = sys.argv[index+1]
+else:
+    noAvg = 1000
+if "-g" in sys.argv:
+    index = sys.argv.index("-g")
+    geometry = sys.argv[index+1]
+else:
+    geometry = 'squared'
+
+for L in List:
+    for p in plist:
+        slanzarv_STR = (
+            f'slanzarv -m {slanzarv_MEMMB} --nomail --jobname "{programName}_{L}_{p:.3g}_{geometry}_{noAvg}"'
+            if slanzarv_OPT
+            else ''
+        )
+        the_string = (
+            slanzarv_STR
+            + f" python src/{programName}.py "
+            + f"{L} {p:.3g} -g {geometry} -nA {noAvg} "
+        )
+        if do_print:
+            print(the_string)
+        else:
+            print(the_string)
+            os.system(the_string)
