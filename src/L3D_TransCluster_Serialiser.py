a097b286ee6065ca7d658f60e55d8acd0be3f318
giulioiannelli <giulioiannelli.w@gmail.com>
2024-05-23 12:01:42 +0200

    Refactor L3D_TransCluster.py and L3D_TransCluster_Serialiser.py to include default float type option

--- src/L3D_TransCluster_Serialiser.py
+++ src/L3D_TransCluster_Serialiser.py
@@ -1,15 +1,103 @@
-import numpy as np
-import os
+# import numpy as np
+# import os
 
-s = [5, 10, 20, 30, 40, 50]
-p = np.linspace(0.15, 0.25, num=10)
-printb = True
+# s = [5, 10, 20, 30, 40, 50]
+# p = np.linspace(0.15, 0.25, num=10)
+# printb = True
 
-for side in s:
-    for pflip in p:
-        execstr = f"slanzarv -m 16384 --nomail python src/L3D_TransCluster.py "\
-            + f"{side} {pflip:.3g} -n 10000 --mode=pCluster"
-        if printb:
-            print(execstr)
-        else:
-            os.system(execstr)
+# for side in s:
+#     for pflip in p:
+#         execstr = f"slanzarv -m 16384 --nomail python src/L3D_TransCluster.py "\
+#             + f"{side} {pflip:.3g} -n 10000 --mode=pCluster"
+#         if printb:
+#             print(execstr)
+#         else:
+#             os.system(execstr)
+
+from parsers.L3D_TransCluster_Serialiser import *
+#
+args = parser.parse_args()
+#
+progName = L3D_TransCluster_progName
+progNameShrt = L3D_TransCluster_progNameShrt
+navg = args.number_of_averages
+fullMode = args.mode
+progMode = args.mode.split('_')[-1]
+execBool = args.exec
+printBool = args.print
+typf = args.float_type
+outsx = args.out_suffix
+
+#
+if fullMode.endswith('pCluster'):
+    List = [10, 20, 30]
+    geo = args.geometry
+    cell = args.cell_type
+    plist = np.linspace(0.06, .3, num=20)
+elif fullMode.endswith('ordParam'):
+    List = [10, 20, 30]
+    def linspacepfunc(*_):
+        return np.linspace(0, 0.5, 100)
+    # geometry_cell_dict = {'squared': L2D_RAND_CELL_LIST,
+    #                     'triangular': L2D_RAND_CELL_LIST,
+    #                     'hexagonal': L2D_RAND_CELL_LIST}
+    geometry_cell_dict = {'simple_cubic': ['rand']}
+    plist = {geo: {cell: linspacepfunc(geo, cell) for cell in cells}  
+            for geo,cells in geometry_cell_dict.items()}
+#
+if fullMode.startswith('slanzarv'):
+    if args.slanzarv_minMB == args.slanzarv_maxMB:
+        def memoryfunc(*_):
+            return args.slanzarv_minMB
+    else:
+        def memoryfunc(x):
+            return int(np.interp(x, [min(List), max(List)], 
+                             [args.slanzarv_minMB, args.slanzarv_maxMB]))
+    def slanzarv_str(mode, L, p, geo, c):
+        slanzarvopt = "--nomail --jobname "
+        slanzarvstr = f"slanzarv -m {memoryfunc(L)} {slanzarvopt}"
+        argstr = f"{progNameShrt}{mode}_{L}_{p:.3g}_{geo[:3]}_{c[3:]}"
+        return slanzarvstr + argstr 
+else:
+    def slanzarv_str(*_):
+        return ""
+#
+if execBool or printBool:
+    if execBool and printBool:
+        def operate(s, count):
+            print(s)
+            os.system(s)
+            return count +1
+    elif execBool:
+        def operate(s, count):
+            os.system(s)
+            return count +1
+    elif printBool:
+        def operate(s, *args):
+            print(s)
+    def exec_str(L, p, geo, cell, navg, mode):
+        lnchStr = f"python src/{progName}.py"
+        argstr = (f"{L} {p:.3g} -g {geo} -c {cell} -n {navg} -t {typf} -o {outsx} "+
+                  f"--mode={mode}")
+        return (f"{slanzarv_str(mode, L, p, geo, cell)} "
+                        f"{lnchStr} {argstr}")
+else:
+    exit(0)
+
+count = 0
+if fullMode.endswith('pCluster'):
+    for L in List:
+        for p in plist:
+            estring = exec_str(L, p, geo, cell, navg, progMode)
+            count = operate(estring, count)
+elif fullMode.endswith('ordParam'):
+    for L in List:
+        for geo, cellst in geometry_cell_dict.items():
+            for cell in cellst:
+                for p in plist[geo][cell]:
+                    estring = exec_str(L, p, geo, cell, navg, progMode)
+                    count = operate(estring, count)
+else:
+    print(f"no program executed, unkonw mode provided: {fullMode}")
+    exit(0)         
+print("submitted jobs: ", count)
