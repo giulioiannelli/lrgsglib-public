fe6b63356ee5366e73444afaad0bf8e62dafc2fc
giulioiannelli <giulioiannelli.w@gmail.com>
2024-05-23 12:02:02 +0200

    Refactor test files and remove unused code

--- test/test/mylib_py.cpp
+++
@@ -1,9 +0,0 @@
-#include <boost/python.hpp>
-#include "mylib.hpp"
-
-BOOST_PYTHON_MODULE(mylib_ext) {
-    using namespace boost::python;
-    class_<MyLib>("MyLib")
-        .def("add", &MyLib::add)
-    ;
-}
--- test/test/use_mylib.py
+++
@@ -1,5 +0,0 @@
-import mylib_ext
-
-obj = mylib_ext.MyLib()
-result = obj.add(2, 3)
-print(f"The result is: {result}")
