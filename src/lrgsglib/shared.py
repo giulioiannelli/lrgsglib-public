b945fcc493aeeb7e9a85ef68556cb1a3278c75b7
giulioiannelli <giulioiannelli.w@gmail.com>
2024-06-24 02:53:09 +0200

    Refactor code to remove unused imports and improve module organization

--- src/lrgsglib/shared.py
+++ src/lrgsglib/shared.py
@@ -8,6 +8,7 @@ import random
 import re
 import string
 import struct
+import subprocess
 import sys
 import time
 #
@@ -28,6 +29,13 @@ from os import chdir, getcwd
 from os.path import join as pth_join
 from scipy.interpolate import pchip
 from scipy.ndimage import gaussian_filter 
-from scipy.sparse import csr_array
+from scipy.cluster.hierarchy import fcluster, dendrogram, linkage
+from scipy.linalg import expm, fractional_matrix_power
+from scipy.ndimage import gaussian_filter1d, gaussian_filter
+from scipy.signal import argrelextrema, medfilt
+from scipy.spatial.distance import squareform
+from scipy.sparse import csr_array, spdiags
+from scipy.sparse import identity as scsp_identity
+from scipy.sparse.linalg import eigsh as scsp_eigsh
 from tqdm import tqdm
 from typing import Any, Optional, Union, List, Tuple, Dict
