1f95445685a2efb93e2a466fe88b17feba656168
giulioiannelli <giulioiannelli.w@gmail.com>
2024-06-24 22:37:37 +0200

    Refactor code to improve module organization and handle file removal gracefully

--- src/lrgsglib/config/const_plotlib.py
+++ src/lrgsglib/config/const_plotlib.py
@@ -1,9 +1,25 @@
-import numpy as np
+from ..shared import *
+#
+import matplotlib.animation as animation
 import matplotlib.cm as cm
-from matplotlib.cm import twilight
-from matplotlib.colors import LinearSegmentedColormap, ListedColormap
+import matplotlib.colors as mplc
+import matplotlib.gridspec as gs
+import matplotlib.pyplot as plt
+#
+from matplotlib import gridspec, rc_context
+from matplotlib.axes import Axes
+from matplotlib.cm import hsv, twilight, ScalarMappable
+from matplotlib.colors import Colormap, ListedColormap, BoundaryNorm
+from matplotlib.colors import LinearSegmentedColormap, Normalize
+from matplotlib.patches import Circle, Rectangle, Ellipse, PathPatch
+from matplotlib.path import Path
+from matplotlib.text import Text
+from matplotlib.ticker import ScalarFormatter, MultipleLocator
+#
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from mpl_toolkits.axes_grid1.axes_divider import AxesDivider
 from mpl_toolkits.axes_grid1.inset_locator import inset_axes, mark_inset
-
+from mpl_toolkits.mplot3d import Axes3D
 
 twilight_lim_low = 0.2
 twilight_lim_high = 0.8
@@ -22,7 +38,6 @@ cm.register_cmap(name='restr_twilight_r', cmap=restr_twilight.reversed())
 red_blue = LinearSegmentedColormap.from_list("red_blue", ["red", "blue"])
 cm.register_cmap(name="red_blue", cmap=red_blue)
 cm.register_cmap(name="red_blue_r", cmap=red_blue.reversed())
-
 
 PLT_SL2DSQ_SIDE1 = 7
 PLT_SL2DSQ_SIDE2 = 7
