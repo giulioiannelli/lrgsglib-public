508dab384dbd2f7d997814b9842f0d487d3484e4
giulioiannelli <giulioiannelli.w@gmail.com>
2024-10-03 19:39:26 +0200

    Refactor code to update method name in FullyConnected.py Refactor code to update method name in SignedRW.py Refactor code to update method name in VoterModel.py Refactor code to update method name in BinDynSys.py Refactor code to update constant names in const.py Refactor code to update method name in Lattice2D.py Remove unused function in funcs.py

--- src/lrgsglib/nx_patches/ErdosRenyi.py
+++ src/lrgsglib/nx_patches/ErdosRenyi.py
@@ -35,18 +35,36 @@ class ErdosRenyi(SignedGraph):
             self.update((key, constant) for key in iterable)
             self.er = er
             self.rd = self.er.GraphReprs
-        #     #
-        #     self['single'] = {g: [self.centedge[g]] for g in self.rd}
-        #     self['singleZERR'] = {g: self.get_links_ZERR(
-        #         self.centedge[g][0], g, self.l.geo) for g in self.rd}
-        #     self['singleXERR'] = {g: self.get_links_XERR(
-        #         self.centedge[g][0], g) for g in self.rd}
-        #     self['rand'] = {g: [e for e in self.l.fleset[g]] 
-        #                     for g in self.rd}
-        #     self['randZERR'] = {g: self.get_rand_pattern('ZERR', on_g=g) 
-        #                            for g in self.rd}
-        #     self['randXERR'] = {g: self.get_rand_pattern('XERR', on_g=g) 
-        #                            for g in self.rd}
-        # #
-        # def get_links_XERR(self, node: Any, on_g: str = L2D_ONREP):
-        #     return [(node, nn) for nn in self.l.graph_neighbors(node, on_g)]
+            self.rNodeFlip = {g: random.sample(
+                                    list(self.er.nodesIn[g]), 
+                                    self.er.nflip
+                                ) for g in self.rd}
+            self['randXERR'] = {g: self.get_rand_pattern('XERR', on_g=g) 
+                                   for g in self.rd}
+        #
+        def get_links_XERR(self, node: Any, on_g: str = ER_ONREP):
+            return [(node, nn) for nn in self.er.get_graph_neighbors(node, on_g)]
+        #
+        def get_rand_pattern(self, mode: str, on_g: str = ER_ONREP):
+            match mode:
+                case 'XERR':
+                    if COUNT_XERR_PATTERNS:
+                        patternList = [k for i in self.rNodeFlip[on_g] 
+                                    for k in self.get_links_XERR(i, on_g)]
+                    else:
+                        tmplst = self.rNodeFlip[on_g]
+                        grph = self.er.gr[on_g]
+                        _ = 0
+                        patternList = []
+                        while _ < len(tmplst):
+                            leval = [all([nnn['weight'] == -1 
+                                        for nnn in grph[nn].values()])
+                                        for nn in grph.neighbors(tmplst[_])]
+                            if any(leval):
+                                tmplst.pop(_)  # Removing the element
+                            else:
+                                glXERR = self.get_links_XERR(tmplst[_], 
+                                                             on_g)
+                                patternList.extend([k for k in glXERR])
+                                _ += 1
+            return patternList
