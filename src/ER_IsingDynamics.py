77e3dde5464718bab65d36f3061c844286d1503f
giulioiannelli <giulioiannelli.w@gmail.com>
2024-10-09 16:38:28 +0200

    Refactor print_chrono option in ER_IsingDynamics.py

--- src/ER_IsingDynamics.py
+++ src/ER_IsingDynamics.py
@@ -69,6 +69,7 @@ for _ in range(navg):
     if remove_files:
         isdy.remove_run_c_files(remove_stderr=True)
         er.remove_edgl_file()
-# Chronometer.print_all_chronometers()
+if print_chrono:
+    Chronometer.print_all_chronometers()
 
 print("Done!")
