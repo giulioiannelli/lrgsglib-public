d418e2cdb3330cddbaafeb8ac96a924c54daeb46
giulioiannelli <giulioiannelli.w@gmail.com>
2024-10-21 12:38:18 +0200

    Refactor L2D_SlaplSpect.py and parsers/L2D_SlaplSpect.py for improved computation of eigenvalues and eigenvectors

--- src/L2D_SlaplSpect.py
+++ src/L2D_SlaplSpect.py
@@ -13,49 +13,85 @@ period = args.period
 bins_count = args.bins_count
 howmany = args.howmany
 #
+#
 l = Lattice2D(side1=side, pflip=p, geo=geo, sgpath=args.workDir)
 workDir = l.spectpath
 fnameBase = '_'.join([f"dist{howmany}", f"{p:.3g}", eigmode])
 fname = workDir+fnameBase+f"_{navg}.pkl"
 #
 #
-initial_eig = np.abs(eigV_for_lattice2D_ptch(side=side, pflip=p, geo=geo, 
-                                             mode=eigmode, howmany=howmany))
-bins, bin_centers = create_symmetric_log_bins(np.min(initial_eig), 
-                                              np.max(initial_eig), 
-                                              bins_count)
-bin_counter = [Counter() for i in range(howmany)]
-fnameOld = ""
-if not os.path.exists(fname):
-    nAvgDone = 0
-    fnameExists = glob.glob(f"{workDir}{fnameBase}*")
-    if fnameExists:
-        nAvgDone = int(os.path.splitext(fnameExists[0].split('_')[-1])[0])
-        fnameOld = fnameExists[0]
-    nAvgNeed = navg - nAvgDone
-    for current_period in range((nAvgNeed // period) + bool(nAvgNeed % period)):
-        batch_size = min(nAvgNeed - current_period * period, period)
-        eig_values = [[] for i in range(howmany)]
-        for _ in range(batch_size):
-            eigV = eigV_for_lattice2D_ptch(side=side, pflip=p, geo=geo, 
-                                                mode=eigmode, howmany=howmany)
-            for i in range(howmany):
-                eig_values[i].append(eigV[i])
-        eig_values = [np.concatenate(i) for i in eig_values]
-        for i in range(howmany):
-            bin_counter[i].update(bin_eigenvalues(eig_values[i], bins, bin_centers))
-        fnameNew = f"{workDir}{fnameBase}_{nAvgDone + (current_period + 1) * period}.pkl"
-        try:
-            os.rename(fnameOld, fnameNew)
-        except FileNotFoundError:
-            pass
-        with open(fnameNew, "wb") as f:
-            pk.dump(bin_counter, f)
-        fnameOld = fnameNew
+match mode:
+    case "eigvec_dist":
+        initial_eig = np.abs(eigV_for_lattice2D_ptch(side=side, pflip=p, geo=geo, 
+                                                     mode=eigmode, howmany=howmany))
+        bins, bin_centers = create_symmetric_log_bins(np.min(initial_eig), 
+                                                     np.max(initial_eig), 
+                                                     bins_count)
+        bin_counter = [Counter() for i in range(howmany)]
+        fnameOld = ""
+        if not os.path.exists(fname):
+            nAvgDone = 0
+            fnameExists = glob.glob(f"{workDir}{fnameBase}*")
+            if fnameExists:
+                nAvgDone = int(os.path.splitext(fnameExists[0].split('_')[-1])[0])
+                fnameOld = fnameExists[0]
+            nAvgNeed = navg - nAvgDone
+            for current_period in range((nAvgNeed // period) + bool(nAvgNeed % period)):
+                batch_size = min(nAvgNeed - current_period * period, period)
+                eig_values = [[] for i in range(howmany)]
+                for _ in range(batch_size):
+                    eigV = eigV_for_lattice2D_ptch(side=side, pflip=p, geo=geo, 
+                                                   mode=eigmode, howmany=howmany)
+                    for i in range(howmany):
+                        eig_values[i].append(eigV[i])
+                eig_values = [np.concatenate(i) for i in eig_values]
+                for i in range(howmany):
+                    bin_counter[i].update(bin_eigenvalues(eig_values[i], bins, bin_centers))
+                fnameNew = f"{workDir}{fnameBase}_{nAvgDone + (current_period + 1) * period}.pkl"
+                try:
+                    os.rename(fnameOld, fnameNew)
+                except FileNotFoundError:
+                    pass
+                with open(fnameNew, "wb") as f:
+                    pk.dump(bin_counter, f)
+                fnameOld = fnameNew
 
-    # At the end, save the final state if needed
-    if nAvgNeed % period:
-        fnameNew = workDir+fnameBase+f"_{navg}.pkl"
-        os.rename(fnameOld, fnameNew)
-        with open(fnameNew, "wb") as f:
-            pk.dump(bin_counter, f)
+            # At the end, save the final state if needed
+            if nAvgNeed % period:
+                fnameNew = workDir+fnameBase+f"_{navg}.pkl"
+                os.rename(fnameOld, fnameNew)
+                with open(fnameNew, "wb") as f:
+                    pk.dump(bin_counter, f)
+
+    case "eigval_dist":
+        all_eigv = []
+        fnameOld = ""
+        if not os.path.exists(fname):
+            nAvgDone = 0
+            fnameExists = glob.glob(f"{workDir}{fnameBase}*")
+            if fnameExists:
+                nAvgDone = int(os.path.splitext(fnameExists[0].split('_')[-1])[0])
+                fnameOld = fnameExists[0]
+            nAvgNeed = navg - nAvgDone
+            for current_period in range((nAvgNeed // period) + bool(nAvgNeed % period)):
+                batch_size = min(nAvgNeed - current_period * period, period)
+                for _ in range(batch_size):
+                    l = Lattice2D(side, geo=geo, pflip=p, init_nw_dict=True)
+                    l.flip_sel_edges(l.nwDict[cell]['G'])
+                    l.compute_k_eigvV(MODE_dynspec='numpy')
+                    all_eigv.extend(l.eigv)
+                fnameNew = f"{workDir}{fnameBase}_{nAvgDone + (current_period + 1) * period}.pkl"
+                try:
+                    os.rename(fnameOld, fnameNew)
+                except FileNotFoundError:
+                    pass
+                with open(fnameNew, "wb") as f:
+                    pk.dump(all_eigv, f)
+                fnameOld = fnameNew
+
+            # At the end, save the final state if needed
+            if nAvgNeed % period:
+                fnameNew = workDir+fnameBase+f"_{navg}.pkl"
+                os.rename(fnameOld, fnameNew)
+                with open(fnameNew, "wb") as f:
+                    pk.dump(all_eigv, f)
