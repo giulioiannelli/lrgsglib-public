aebcbba09889298189b234c7800b2bfff93422dc
giulioiannelli <giulioiannelli.w@gmail.com>
2024-10-30 15:11:26 +0100

    Refactor L2D_SlaplSpect.py to improve modularity and efficiency in eigvec_dist and eigval_dist modes

--- src/L2D_SlaplSpect.py
+++ src/L2D_SlaplSpect.py
@@ -20,22 +20,22 @@ workDir = l.spectpath
 #
 match mode:
     case "eigvec_dist":
-        fnameBase = '_'.join([f"dist{howmany}", f"{p:.3g}", eigmode])
-        fname = workDir+fnameBase+f"_{navg}.pkl"
+        fname_base = '_'.join([f"dist{howmany}", f"{p:.3g}", eigmode])
+        fname = workDir+fname_base+f"_{navg}.pkl"
         initial_eig = np.abs(eigV_for_lattice2D_ptch(side=side, pflip=p, geo=geo, 
                                                      mode=eigmode, howmany=howmany))
         bins, bin_centers = create_symmetric_log_bins(np.min(initial_eig), 
                                                      np.max(initial_eig), 
                                                      bins_count)
         bin_counter = [Counter() for i in range(howmany)]
-        fnameOld = ""
+        fname_old = ""
         if not os.path.exists(fname):
-            nAvgDone = 0
-            fnameExists = glob.glob(f"{workDir}{fnameBase}*")
-            if fnameExists:
-                nAvgDone = int(os.path.splitext(fnameExists[0].split('_')[-1])[0])
-                fnameOld = fnameExists[0]
-            nAvgNeed = navg - nAvgDone
+            navg_done = 0
+            globfname_exists = glob.glob(f"{workDir}{fname_base}*")
+            if globfname_exists:
+                navg_done = int(os.path.splitext(globfname_exists[0].split('_')[-1])[0])
+                fname_old = globfname_exists[0]
+            nAvgNeed = navg - navg_done
             for current_period in range((nAvgNeed // period) + bool(nAvgNeed % period)):
                 batch_size = min(nAvgNeed - current_period * period, period)
                 eig_values = [[] for i in range(howmany)]
@@ -47,53 +47,54 @@ match mode:
                 eig_values = [np.concatenate(i) for i in eig_values]
                 for i in range(howmany):
                     bin_counter[i].update(bin_eigenvalues(eig_values[i], bins, bin_centers))
-                fnameNew = f"{workDir}{fnameBase}_{nAvgDone + (current_period + 1) * period}.pkl"
+                fnameNew = f"{workDir}{fname_base}_{navg_done + (current_period + 1) * period}.pkl"
                 try:
-                    os.rename(fnameOld, fnameNew)
+                    os.rename(fname_old, fnameNew)
                 except FileNotFoundError:
                     pass
                 with open(fnameNew, "wb") as f:
                     pk.dump(bin_counter, f)
-                fnameOld = fnameNew
+                fname_old = fnameNew
 
             # At the end, save the final state if needed
             if nAvgNeed % period:
-                fnameNew = workDir+fnameBase+f"_{navg}.pkl"
-                os.rename(fnameOld, fnameNew)
+                fnameNew = workDir+fname_base+f"_{navg}.pkl"
+                os.rename(fname_old, fnameNew)
                 with open(fnameNew, "wb") as f:
                     pk.dump(bin_counter, f)
-
-    case "eigval_dist":
-        all_eigv = []
-        fnameOld = ""
-        fnameBase = '_'.join([f"dist_eigval", f"{p:.3g}", cell])
-        fname = workDir+fnameBase+f"_{navg}.pkl"
+    case 'eigval_dist':
+        fname_old = ''
+        fname_base = '_'.join([f"dist_eigval", f"{p:.3g}", cell])
+        fname_suffix = f"_{navg}.pkl"
+        fname = workDir+fname_base+fname_suffix
         if not os.path.exists(fname):
-            nAvgDone = 0
-            fnameExists = glob.glob(f"{workDir}{fnameBase}*")
-            if fnameExists:
-                nAvgDone = int(os.path.splitext(fnameExists[0].split('_')[-1])[0])
-                fnameOld = fnameExists[0]
-            nAvgNeed = navg - nAvgDone
+            navg_done = 0
+            globfname_exists = glob.glob(f"{workDir}{fname_base}*")
+            if globfname_exists:
+                navg_done = int(os.path.splitext(globfname_exists[0].split('_')[-1])[0])
+                fname_old = globfname_exists[0]
+            nAvgNeed = navg - navg_done
+            #
+            all_eigv = []
             for current_period in range((nAvgNeed // period) + bool(nAvgNeed % period)):
                 batch_size = min(nAvgNeed - current_period * period, period)
                 for _ in range(batch_size):
                     l = Lattice2D(side, geo=geo, pflip=p, init_nw_dict=True)
                     l.flip_sel_edges(l.nwDict[cell]['G'])
-                    l.compute_k_eigvV(MODE_dynspec='numpy')
+                    l.compute_full_laplacian_spectrum()
                     all_eigv.extend(l.eigv)
-                fnameNew = f"{workDir}{fnameBase}_{nAvgDone + (current_period + 1) * period}.pkl"
+                fnameNew = f"{workDir}{fname_base}_{navg_done + (current_period + 1) * period}.pkl"
                 try:
-                    os.rename(fnameOld, fnameNew)
+                    os.rename(fname_old, fnameNew)
                 except FileNotFoundError:
                     pass
                 with open(fnameNew, "wb") as f:
                     pk.dump(all_eigv, f)
-                fnameOld = fnameNew
+                fname_old = fnameNew
 
             # At the end, save the final state if needed
             if nAvgNeed % period:
-                fnameNew = workDir+fnameBase+f"_{navg}.pkl"
-                os.rename(fnameOld, fnameNew)
+                fnameNew = workDir+fname_base+f"_{navg}.pkl"
+                os.rename(fname_old, fnameNew)
                 with open(fnameNew, "wb") as f:
                     pk.dump(all_eigv, f)
