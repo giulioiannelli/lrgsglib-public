493a222ad81860cb234dec574e2078494421363d
giulioiannelli <giulioiannelli.w@gmail.com>
2024-11-10 19:47:54 +0100

    Fixing ER_IsingDynamics.py to streamline ground state handling and update compute_k_eigvV method call

--- src/ER_IsingDynamics.py
+++ src/ER_IsingDynamics.py
@@ -13,55 +13,26 @@ ic = args.init_cond
 navg = args.number_of_averages
 navg2 = args.number_of_averages2
 in_suffix = args.in_suffix
-out_suffix = args.out_suffix
 NoClust = args.NoClust
 runlang = args.runlang
 remove_files = args.remove_files
 workdir = args.workdir
 print_chrono = args.print_chrono
-if ic.startswith('ground_state'):
-    parts = ic.split('_')
-    number = int(parts[-1])
-    howmany = number+1
-else:
-    number  = 0
-    howmany = 1
+ic_gs = ic.startswith('ground_state')
+number = int(ic.split('_')[-1]) if ic_gs else 0
+out_suffix = args.out_suffix or "gs"+str(number) if ic_gs else ic
+
 #
 erDictArgs = dict(n=N, p=p, sgpath=workdir, pflip=pflip, init_nw_dict=True)
 isingDictArgs = dict(T=T, ic=ic, runlang=runlang, NoClust=NoClust, rndStr=True, 
                      out_suffix=out_suffix, id_string=in_suffix)
-# er = ErdosRenyi(**erDictArgs)
-# Fname = f'GC_meanVar_p={pflip:.3g}_{cell}_{in_suffix}.txt'
-# pathFname = os.path.join(er.expOut, Fname)
-# if not os.path.exists(pathFname):
-#     lenList = []
-#     for _ in range(navg2):
-#         er = ErdosRenyi(**erDictArgs)
-#         er.flip_sel_edges(er.nwDict[cell]['G'])
-#         # er.flip_random_fract_edges()
-#         er.compute_k_eigvV(howmany=number + 1)
-#         er.load_eigV_on_graph(which=number, binarize=True)
-#         er.make_clustersYN(f'eigV{number}', +1)
-#         lenList.append(len(er.gc))
-#     meanN, stdN = np.mean(lenList)/er.N, np.std(lenList)
-#     np.savetxt(pathFname, np.atleast_2d([meanN, stdN]), fmt='%.3g')
-# else:
-#     meanN, stdN = np.loadtxt(pathFname)
 for _ in range(navg):
-    # iter_gen = 0
-    # while True:
     er = ErdosRenyi(**erDictArgs)
     er.flip_sel_edges(er.nwDict[cell]['G'])
-    # er.flip_random_fract_edges()
-    er.compute_k_eigvV(k=howmany)
+    er.compute_k_eigvV(k=number+1)
     er.load_eigV_on_graph(which=number, binarize=True)
     er.make_clustersYN(f'eigV{number}', +1)
-        # if  abs(len(er.gc)/er.N - meanN) < stdN:
-        #     break
-        # elif iter_gen > DEFAULT_MAX_ITER_ER_GC:
-        #     raise ValueError(f"Exceeded maximum number of iterations\
-        #                       {DEFAULT_MAX_ITER_ER_GC}")
-        # iter_gen += 1
+    #
     isdy = IsingDynamics(er, **isingDictArgs)
     isdy.init_ising_dynamics()
     er.export_edgel_bin(exName=isdy.id_string_isingdyn)
