911cebdf313c5865f1f3d12d76d171f62cb66d35
giulioiannelli <giulioiannelli.w@gmail.com>
2024-11-11 16:11:20 +0100

    Remove unused Code_PhT_Glass.py and related test scripts to streamline the repository

--- test/hex_xerr_phtraPablo.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import numpy as np
-
-script_path = "src/Code_PhT_Glass.py"
-
-for p in np.linspace(0, 0.5, 20):
-	args = ["32", f"{p:.3g}"]
-	subprocess.run(["python", script_path] + args)
