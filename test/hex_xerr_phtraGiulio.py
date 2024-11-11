53631f2b6f26a55017e3a69ca69e29cdc9677ecf
giulioiannelli <giulioiannelli.w@gmail.com>
2024-11-11 16:11:20 +0100

    Remove unused Code_PhT_Glass.py and related test scripts to streamline the repository

--- test/hex_xerr_phtraGiulio.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import numpy as np
-
-script_path = "src/L2D_TransCluster.py"
-
-for p in np.linspace(0, 0.5, 20):
-	args = ["32", f"{p:.3g}", "-c", "randXERR", "-n", "50", "--outdir", 
-		"data/test_hexerr/", "-g", "hex"]
-	subprocess.run(["python", script_path] + args)
