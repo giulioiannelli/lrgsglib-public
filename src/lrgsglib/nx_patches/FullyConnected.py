3bf8b387675224a2b439894245e01b4924c5a7aa
giulioiannelli <giulioiannelli.w@gmail.com>
2024-12-17 16:21:55 +0100

    Refactor FullyConnected.py: enhance initialization and edge computation methods for improved functionality

--- src/lrgsglib/nx_patches/FullyConnected.py
+++ src/lrgsglib/nx_patches/FullyConnected.py
@@ -3,25 +3,65 @@ from .funcs import *
 from .SignedGraph import SignedGraph
 from ..config.plotlib import Colormap
 class FullyConnected(SignedGraph):
-    def __init__(self, side1: int, anigemb: str = "sle"):
-        self.side1 = side1
-        self.G = nx.complete_graph(self.side1)
-        super(FullyConnected, self).__init__(self.G)
-        self.init_graph()
-        self.pbc = True
-        self.DEFAULT_NEG_WEIGHTS_DICT_G = {self.esetG[len(self.esetG) // 2]: -1}
-        self.DEFAULT_NEG_WEIGHTS_DICT_H = self.DEFAULT_NEG_WEIGHTS_DICT_G
+    #
+    def __init__(
+        self, 
+        N: int = FC_N,
+        with_positions: bool = False,
+        mode_positions: Union[str, Callable] = "circular",
+        anigemb: str = "sle",
+        **kwargs
+    ) -> None:
+        self.N = N
+        self.with_positions = with_positions
+        self.mode_positions = mode_positions
+        self.__init_fullyconnected__()
+        super(FullyConnected, self).__init__(self.G, **kwargs)
         self.animation_graph_embedding = anigemb
-
-    def init_graph(self):
-        self.H = self.G
-        self.upd_graph_matrices()
-
-    def init_paths(self):
-        self.sgpathn = f"fc/"
-        self.pltPath = f"data/plot/{self.sgpathn}"
-        self.datPath = f"data/{self.sgpathn}"
-
+    #
+    def __init_fullyconnected__(self) -> None:
+        self.G = nx.complete_graph(self.N)
+        if self.with_positions:
+            match self.mode_positions:
+                case "circular":
+                    pos = nx.circular_layout(self.G)
+                case "sspectral":
+                    pos = signed_spectral_layout(self.G)
+                case callable(mode) if callable(mode):
+                    pos = mode(self.G)
+                case _:
+                    raise ValueError(f"Unsupported mode_positions: {self.mode_positions}")
+            nx.set_node_attributes(self.G, pos, "pos")
+    #
+    def __repr__(self) -> str:
+        return f"FullyConnected(N={self.N})"
+    #
+    def __str__(self) -> str:
+        return f"FullyConnected(N={self.N})"
+    #
+    def compute_hopfield_edges(self, mode: str) -> None:
+        if mode == "random":
+            self.hopfield_edges = {
+                (u, v): np.random.choice([-1, 1])
+                for u, v in self.G.edges()
+            }
+        elif mode == "all+":
+            self.hopfield_edges = {
+                (u, v): 1
+                for u, v in self.G.edges()
+            }
+        elif mode == "all-":
+            self.hopfield_edges = {
+                (u, v): -1
+                for u, v in self.G.edges()
+            }
+        else:
+            raise ValueError(f"Unsupported mode: {mode}")
+    #
+    def set_hopfield_edges(self, on_g: str = SG_GRAPH_REPR, **kwcompute_hopf_edges) -> None:
+        if not hasattr(self, "hopfield_edges"):
+            self.compute_hopfield_edges(**kwcompute_hopf_edges)
+        nx.set_edge_attributes(self.gr[on_g], self.hopfield_edges, "weight")
     #
     def make_animation(
         self, fig, ax, frames, cmap: Union[str, Colormap] = "viridis"
