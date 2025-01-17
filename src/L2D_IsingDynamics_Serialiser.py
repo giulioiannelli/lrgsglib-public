f8497e705ae0f573a4be319892b18724899092b8
giulioiannelli <giulioiannelli.w@gmail.com>
2025-01-17 15:02:57 +0100

    Update phase diagram notebook and serialiser: adjust execution counts, refine parameter lists, and enhance plotting logic for improved clarity and functionality

--- src/L2D_IsingDynamics_Serialiser.py
+++ src/L2D_IsingDynamics_Serialiser.py
@@ -21,7 +21,7 @@ progNameShrt = L2D_IsingDynamics_progNameShrt
 execBool = args.exec
 printBool = args.print
 #
-List = [16, 32, 48, 64, 96, 128]
+List = [16, 32, 64, 96, 128]
 navglist = [navg for i in range(len(List))]#[navg//(2**i) for i in range(len(List))]
 plist = np.linspace(0.01, 0.5, num=25)
 Tlist = np.concatenate([np.linspace(0.1, 1.6, 15),
