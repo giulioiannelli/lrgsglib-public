f3ef8b5eba8c0535b6ebcc7fa505b28921626223
giulioiannelli <giulioiannelli.w@gmail.com>
2025-01-24 12:16:33 +0100

    Launch of the simulations

--- src/kernels/L2D_IsingDynamics.py
+++ src/kernels/L2D_IsingDynamics.py
@@ -13,12 +13,13 @@ def get_out_suffix(args, ic_gs, number):
     return args.out_suffix or "gs"+str(number)+args.cell_type if ic_gs else args.init_cond
 
 def run_simulation(args, l2dDictArgs, isingDictArgs, number, remove_files, min_gc_cond=True):
-    MAX_COUNT = 1e5
+    MAX_COUNT = 1e2
+    LENGC_MIN = 0.33
     for _ in range(args.number_of_averages):
         if args.runlang == 'C4':
             lengc = 0
             count = 0
-            while lengc < 0.33 or count < MAX_COUNT:
+            while lengc < LENGC_MIN or count < MAX_COUNT:
                 seedsave = np.random.randint(0, 2**32-1)
                 lattice = Lattice2D(seed=seedsave, **l2dDictArgs)
                 lattice.flip_sel_edges(lattice.nwDict['randXERR']['G'])
@@ -27,7 +28,9 @@ def run_simulation(args, l2dDictArgs, isingDictArgs, number, remove_files, min_g
                 lattice.make_clustersYN(f"eigV{number}", val=-1)
                 lengc = len(lattice.gc)/lattice.N
                 count += 1
-            
+            if count == MAX_COUNT:
+                LENGC_MIN -= 0.01
+
         else:
             lattice = Lattice2D(**l2dDictArgs)
             lattice.flip_sel_edges(lattice.nwDict[args.cell_type]['G'])
