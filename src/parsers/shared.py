2502153c5092e109b9993bfac16d1b130452498e
giulioiannelli <giulioiannelli.w@gmail.com>
2025-02-03 14:44:28 +0100

    Add parsDict_get function to retrieve values from parameter dictionary

--- src/parsers/shared.py
+++ src/parsers/shared.py
@@ -3,3 +3,6 @@ import argparse
 def parse_arguments(parser: argparse.ArgumentParser) -> argparse.Namespace:
     args = parser.parse_args()
     return args
+
+def parsDict_get(parsDict, var, key):
+    return parsDict[var].get(key, None)
