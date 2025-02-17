47348bad43811001637907c069385285f4dbfab4
giulioiannelli <giulioiannelli.w@gmail.com>
2025-02-17 18:50:07 +0100

    Refactor L2D_IsingDynamics_Serialiser.py for improved variable naming and clarity; update exec_str function to enhance argument handling.

--- src/L2D_IsingDynamics_Serialiser.py
+++ src/L2D_IsingDynamics_Serialiser.py
@@ -1,4 +1,5 @@
 from parsers.L2D_IsingDynamics_Serialiser import parse_arguments, parser
+from lrgsglib.config.funcs import *
 from lrgsglib.config.progargs import *
 from lrgsglib.shared import *
 #
@@ -18,26 +19,29 @@ ic_gs = args.init_cond.startswith('ground_state')
 number = int(args.init_cond.split('_')[-1]) if ic_gs else 0
 workdir = args.workdir
 #
-progName = L2D_IsingDynamics_progName
-progNameShrt = L2D_IsingDynamics_progNameShrt
-execBool = args.exec
-printBool = args.print
+progn = L2D_IsingDynamics_progName
+progn_shrt = L2D_IsingDynamics_progNameShrt
+exec_bool = args.exec
+prnt_bool = args.print
 #
-List = args.side1_list
-navglist = [navg for _ in range(len(List))]
-plist = args.pflip_linsp
-Tlist = args.Temp_linsp
+side_list = args.side1_list
+navg_list = [navg for _ in range(len(side_list))]
+pflp_list = args.pflip_linsp
+temp_list = args.Temp_linsp
 
 if args.slanzarv_minMB == args.slanzarv_maxMB:
     def memoryfunc(*_):
         return args.slanzarv_minMB
 else:
     def memoryfunc(x):
-        return np.interp(x, [min(List), max(List)],
-                [args.slanzarv_minMB, args.slanzarv_maxMB]).astype(int)
-def slanzarv_str(mode, L, p, geo, c, T, nomail=True, moretime=False, short=True):
-    jobname = '_'.join([progNameShrt, mode[:3], f"{L}", f"{p:.3g}", f"{T:.3g}", 
-                        geo[:3], out_suffix])
+        hl_side = [min(side_list), max(side_list)]
+        hl_memy = [args.slanzarv_minMB, args.slanzarv_maxMB]
+        return np.interp(x, hl_side, hl_memy).astype(int)
+def slanzarv_str(mode, L, p, geo, c, T, 
+                 nomail=True, moretime=False, short=True):
+    jobname = join_non_empty('_', progn_shrt, mode[:3],
+                            f"{L}", f"{p:.3g}", f"{T:.3g}", geo[:3], c, 
+                            out_suffix)
     slanzarvopt = f"--jobname {jobname} "
     if nomail:
         slanzarvopt += "--nomail "
@@ -48,43 +52,44 @@ def slanzarv_str(mode, L, p, geo, c, T, nomail=True, moretime=False, short=True)
     slanzarvstr = f"slanzarv -m {memoryfunc(L)} {slanzarvopt}"
     return slanzarvstr
 #
-if execBool or printBool:
+if exec_bool or prnt_bool:
     count = 0
     count_exe = 0
     def operate(s):
         global count, count_exe
-        if execBool and printBool:
+        if exec_bool and prnt_bool:
             print(s)
             os.system(s)
             count += 1
             count_exe += 1
-        elif execBool:
+        elif exec_bool:
             os.system(s)
             count_exe += 1
-        elif printBool:
+        elif prnt_bool:
             print(s)
             count += 1
-    def exec_str(L, p, geo, cell, navg, T, runlang, in_suffix, out_suffix, 
+    def exec_str(L, p, geo, cell, navg, T, runlang, in_suffix, out_suffix, ic,
                  NoClust, workdir=workdir):
-        lnchStr = f"python src/{progName}.py"
-        in_suffix = in_suffix or f"p={p:.3g}"
-        outstr = f"-os {out_suffix} " if out_suffix else " "
-        argstr = (f"{L} {p:.3g} {T:.3g} -g {geo} -c {cell} -n {navg} -ic {ic} "
-                  f"-rl {runlang} -is {in_suffix} "
-                  f"{outstr} "
-                  f"-nc {NoClust} "
-                  f"-ts {thrmsteps} "
-                  f"-wd {workdir} " if workdir else "")
-        return f"{slanzarv_str(runlang, L, p, geo, cell, T)} {lnchStr} {argstr}"
+        lnchStr = f"python src/{progn}.py"
+        instr = ' '.join([f"-is ", f"{in_suffix}" or f"p={p:.3g}"])
+        outstr = f"-os {out_suffix} " if out_suffix else ""
+        wdstr = f"-wd {workdir} " if workdir else ""
+        argstr = join_non_empty(' ', f"{L}", f"{p:.3g}", f"{T:.3g}", 
+                                f"-g {geo}", f"-c {cell}", f"-n {navg}", 
+                                f"-ic {ic}", f"-rl {runlang}", instr,
+                                outstr, f"-nc {NoClust}", f"-ts {thrmsteps}", wdstr)
+        slanzarvStr = slanzarv_str(runlang, L, p, geo, cell, T)
+        print(lnchStr, argstr, slanzarvStr)
+        return ' '.join([slanzarvStr, lnchStr, argstr])
 
 else:
     exit(0)
 
-for L,navv in zip(List, navglist):
-    for p in plist:
-            for T in Tlist:
+for L,navv in zip(side_list, navg_list):
+    for p in pflp_list:
+            for T in temp_list:
                 estring = exec_str(L, p, geo, cell, navv, T, 
-                                   runlang, in_suffix, out_suffix, NoClust)
+                                   runlang, in_suffix, out_suffix, ic, NoClust)
                 operate(estring)
 print(f"Total number of jobs executed: {count_exe}")
 print(f"Total number of jobs printed: {count}")
