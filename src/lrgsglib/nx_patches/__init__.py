e8b83905c0ca5fca54a37e3e165459d81d2fca6f
giulioiannelli <giulioiannelli.w@gmail.com>
2025-02-17 17:12:46 +0100

    Add HofieldNN import to nx_patches module

--- src/lrgsglib/nx_patches/__init__.py
+++ src/lrgsglib/nx_patches/__init__.py
@@ -1,6 +1,7 @@
 from .SignedGraph import SignedGraph
 from .ErdosRenyi import ErdosRenyi
 from .FullyConnected import FullyConnected
+from .HofieldNN import HofieldNN
 from .Lattice2D import Lattice2D
 from .Lattice3D import Lattice3D
 from .funcs import *
