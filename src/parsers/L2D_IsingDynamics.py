ae94b28f500bf70592b938acc4809168e754a080
giulioiannelli <giulioiannelli.w@gmail.com>
2025-03-07 16:44:29 +0100

    Refactor L2D_IsingDynamics and L2D_IsingDynamics_Serialiser to streamline argument handling and improve clarity; consolidate optional and action argument definitions.

--- src/parsers/L2D_IsingDynamics.py
+++ src/parsers/L2D_IsingDynamics.py
@@ -2,15 +2,9 @@ from lrgsglib.core import *
 from lrgsglib.config.progargs import *
 from parsers.shared import *
 #
-phelp_L = """
-    Size of the square lattice.
-"""
-phelp_p = """
-    Edge flipping probability.
-"""
-phelp_T = """
-    Temperature of the Ising model.
-"""
+phelp_L = "Size of the square lattice"
+phelp_p = "Edge flipping probability"
+phelp_T = "Temperature of the Ising model"
 #
 parsDict = {
     'L': {'help': phelp_L, 'type': int},
@@ -19,60 +13,76 @@ parsDict = {
 }
 #
 parsDictOpt = {
-    'geo': {'names': ['-g', '--geometry'],
-            'help': phelp_geo,
-            'type': str,
-            'default': DEFAULT_GEO},
-    'cell': {'names': ['-c', '--cell_type'],
-            'help': phelp_cell,
-            'type': str,
-            'default': DEFAULT_CELL},
-    'navg': {'names': ['-n', '--number_of_averages'],
-            'help': phelp_navg,
-            'type': int,
-            'default': DEFAULT_NAVG},
-    'init_cond': {'names': ['-ic', '--init_cond'],
-                    'help': phelp_ic,
-                    'type': str,
-                    'default': DEFAULT_INIT_COND},
-    'runlang': {'names': ['-rl', '--runlang'],
-                'help': phelp_runlang,
-                'type': str,
-                'default': DEFAULT_RUNLANG},
-    'in_suffix': {'names': ['-is', '--in_suffix'],
-                    'help': phelp_insuffix,
-                    'type': str,
-                    'default': DEFAULT_INSFFX},
-    'out_suffix': {'names': ['-os', '--out_suffix'],
-                    'help': phelp_outsuffix,
-                    'type': str,
-                    'default': DEFAULT_OUTSFFX},
-    'NoClust': {'names': ['-nc', '--NoClust'],
-                'help': phelp_NoClust,
-                'type': int,
-                'default': DEFAULT_NOCLUST},
-    'workdir': {'names': ['-wd', '--workdir'],
-                'help': phelp_workdir,
-                'type': str,
-                'default': DEFAULT_WORKDIR},
-    'thrmsteps': {'names': ['-ts', '--thrmsteps'],
-                    'help': phelp_thrmsteps,
-                    'type': int,
-                    'default': DEFAULT_THRMSTEPS}
+    'cell': {
+        'names': ['-c', '--cell_type'],
+        'help': phelp_cell,
+        'type': str,
+        'default': DEFAULT_CELL},
+    'geo': {
+        'names': ['-g', '--geometry'],
+        'help': phelp_geo,
+        'type': str,
+        'default': DEFAULT_GEO},
+    'in_suffix': {
+        'names': ['-is', '--in_suffix'],
+        'help': phelp_insuffix,
+        'type': str,
+        'default': DEFAULT_INSFFX},
+    'init_cond': {
+        'names': ['-ic', '--init_cond'],
+        'help': phelp_ic,
+        'type': str,
+        'default': DEFAULT_INIT_COND},
+    'navg': {
+        'names': ['-n', '--number_of_averages'],
+        'help': phelp_navg,
+        'type': int,
+        'default': DEFAULT_NAVG},
+    'NoClust': {
+        'names': ['-nc', '--NoClust'],
+        'help': phelp_NoClust,
+        'type': int,
+        'default': DEFAULT_NOCLUST},
+    'out_suffix': {
+        'names': ['-os', '--out_suffix'],
+        'help': phelp_outsuffix,
+        'type': str,
+        'default': DEFAULT_OUTSFFX},
+    'runlang': {
+        'names': ['-rl', '--runlang'],
+        'help': phelp_runlang,
+        'type': str,
+        'default': DEFAULT_RUNLANG},
+    'thrmsteps': {
+        'names': ['-ts', '--thrmsteps'],
+        'help': phelp_thrmsteps,
+        'type': int,
+        'default': DEFAULT_THRMSTEPS},
+    'val': {
+        'names': ['-V', '--val'],
+        'help': phelp_val,
+        'type': float,
+        'default': DEFAULT_VAL},
+    'workdir': {
+        'names': ['-wd', '--workdir'],
+        'help': phelp_workdir,
+        'type': str,
+        'default': DEFAULT_WORKDIR},
 }
 #
-parDA = {'remove_files': {'names': ['-rf', '--remove_files'],
-                            'help': phelp_remove_files,
-                            'action': argparse.BooleanOptionalAction,
-                            'default': DEFAULT_REMOVE_FILES},
-        'verbose': {'names': ['-v', '--verbose'],
-                    'help': phelp_verbose,
-                    'action': argparse.BooleanOptionalAction,
-                    'default': DEFAULT_VERBOSE},
-        'print_chrono': {'names': ['-pc', '--print_chrono'],
-                        'help': phelp_print_chrono,
-                        'action': argparse.BooleanOptionalAction,
-                        'default': DEFAULT_PRINT_CHRONO}
+parDA = {
+    'remove_files': {'names': ['-rf', '--remove_files'],
+                     'help': phelp_remove_files,
+                     'action': argparse.BooleanOptionalAction,
+                     'default': DEFAULT_REMOVE_FILES},
+    'verbose': {'names': ['-v', '--verbose'],
+                'help': phelp_verbose,
+                'action': argparse.BooleanOptionalAction,
+                'default': DEFAULT_VERBOSE},
+    'print_chrono': {'names': ['-pc', '--print_chrono'],
+                     'help': phelp_print_chrono,
+                     'action': argparse.BooleanOptionalAction,
+                     'default': DEFAULT_PRINT_CHRONO}
 }
 #
 parser = argparse.ArgumentParser(description=L2D_IsingDynamics_description, 
--- src/parsers/L2D_IsingDynamics_Serialiser.py
+++ src/parsers/L2D_IsingDynamics_Serialiser.py
@@ -2,97 +2,101 @@ from lrgsglib.core import *
 from lrgsglib.config.progargs import *
 from parsers.shared import *
 #
-parserDict_Opt = {
-    'side1_list': {'names': ['-s1', '--side1_list'],
-                   'help': phelp_side1_list,
-                   'type': int,
-                   'nargs': '+',
-                   'default': DEFAULT_SIDE1_LIST},
-    'pflip_linsp': {'names': ['-pFT', '--pflip_linsp'],
-                   'help': phelp_pflip_linsp,
-                   'type': parse_multiple_linspace,
-                   'default': DEFAULT_PFLIP_LINSP},
-    'Temp_linsp': {'names': ['-TT', '--Temp_linsp'],
-                    'help': phelp_Temp_linsp,
-                    'type': parse_multiple_linspace,
-                    'default': DEFAULT_TEMP_LINSP},
-    'geo': {'names': ['-g', '--geometry'],
-            'help': phelp_geo,
-            'type': str,
-            'default': DEFAULT_GEO} ,
-    'cell': {'names': ['-c', '--cell_type'],
-            'help': phelp_cell,
-            'type': str,
-            'default': DEFAULT_CELL},
-    'navg': {'names': ['-n', '--number_of_averages'],
-            'help': phelp_navg,
-            'type': int,
-            'default': DEFAULT_NAVG},
-    'init_cond': {'names': ['-ic', '--init_cond'],
-                    'help': phelp_ic,
-                    'type': str,
-                    'default': DEFAULT_INIT_COND},
-    'runlang': {'names': ['-rl', '--runlang'],
-                'help': phelp_runlang,
-                'type': str,
-                'default': DEFAULT_RUNLANG},
-    'in_suffix': {'names': ['-is', '--in_suffix'],
-                    'help': phelp_insuffix,
-                    'type': str,
-                    'default': DEFAULT_INSFFX},
-    'out_suffix': {'names': ['-os', '--out_suffix'],
-                    'help': phelp_outsuffix,
-                    'type': str,
-                    'default': DEFAULT_OUTSFFX},
-    'NoClust': {'names': ['-nc', '--NoClust'],
-                'help': phelp_NoClust,
-                'type': int,
-                'default': DEFAULT_NOCLUST},
-    'mMB': {'names': ['-mMB', '--slanzarv_minMB'],               
-            'help': phelp_mMB,
-            'type': int,
-            'default': DEFAULT_mMB},
-    'MMB': {'names': ['-MMB', '--slanzarv_maxMB'],
-            'help': phelp_MMB,
-            'type': int,
-            'default': DEFAULT_MMB},
-    'thermsteps': {'names': ['-ts', '--thermsteps'],
-                    'help': phelp_thrmsteps,
-                    'type': int,
-                    'default': DEFAULT_THRMSTEPS},
-    'workdir': {'names': ['-wd', '--workdir'],
-                'help': phelp_workdir,
-                'type': str,
-                'default': DEFAULT_WORKDIR}
+optional_args_dict = {
+    tuple(['-s1', '--side1_list']): {
+        'help': phelp_side1_list,
+        'type': int,
+        'nargs': '+',
+        'default': DEFAULT_SIDE1_LIST
+    },
+    tuple(['-pFT', '--pflip_linsp']): {
+        'help': phelp_pflip_linsp,
+        'type': parse_multiple_linspace,
+        'default': DEFAULT_PFLIP_LINSP
+    },
+    tuple(['-TT', '--Temp_linsp']): {
+        'help': phelp_Temp_linsp,
+        'type': parse_multiple_linspace,
+        'default': DEFAULT_TEMP_LINSP
+    },
+    tuple(['-g', '--geometry']): {
+        'help': phelp_geo,
+        'type': str,
+        'default': DEFAULT_GEO
+    } ,
+    tuple(['-c', '--cell_type']): {
+        'help': phelp_cell,
+        'type': str,
+        'default': DEFAULT_CELL
+    },
+    tuple(['-n', '--number_of_averages']): {
+        'help': phelp_navg,
+        'type': int,
+        'default': DEFAULT_NAVG
+    },
+    tuple(['-ic', '--init_cond']): {
+        'help': phelp_ic,
+        'type': str,
+        'default': DEFAULT_INIT_COND
+    },
+    tuple(['-rl', '--runlang']): {
+        'help': phelp_runlang,
+        'type': str,
+        'default': DEFAULT_RUNLANG
+    },
+    tuple(['-is', '--in_suffix']): {
+        'help': phelp_insuffix,
+        'type': str,
+        'default': DEFAULT_INSFFX
+    },
+    tuple(['-os', '--out_suffix']): {
+        'help': phelp_outsuffix,
+        'type': str,
+        'default': DEFAULT_OUTSFFX
+    },
+    tuple(['-nc', '--NoClust']): {
+        'help': phelp_NoClust,
+        'type': int,
+        'default': DEFAULT_NOCLUST
+    },
+    tuple(['-mMB', '--slanzarv_minMB']): {
+        'help': phelp_mMB,
+        'type': int,
+        'default': DEFAULT_mMB
+    },
+    tuple(['-MMB', '--slanzarv_maxMB']): {
+        'help': phelp_MMB,
+        'type': int,
+        'default': DEFAULT_MMB
+    },
+    tuple(['-ts', '--thermsteps']): {
+        'help': phelp_thrmsteps,
+        'type': int,
+        'default': DEFAULT_THRMSTEPS
+    },
+    tuple(['-wd', '--workdir']): {
+        'help': phelp_workdir,
+        'type': str,
+        'default': DEFAULT_WORKDIR
+    }
 }
-parDA = {'exec': {'names': ['-e', '--exec'],
-                        'help': phelp_exc,
-                        'action': argparse.BooleanOptionalAction,
-                        'default': DEFAULT_EXEC},
-                'print': {'names': ['-p', '--print'],                 
-                        'help': phelp_print,
-                        'action': argparse.BooleanOptionalAction,
-                        'default': DEFAULT_PRINT}
-                        }
+action_args_dict = {
+    tuple(['--exec']): {
+        'help': phelp_exc,
+        'action': argparse.BooleanOptionalAction,
+        'default': DEFAULT_EXEC
+    },
+    tuple(['--print']): {
+        'help': phelp_print,
+        'action': argparse.BooleanOptionalAction,
+        'default': DEFAULT_PRINT
+    }
+}
 # Setup the argument parser
-parser = argparse.ArgumentParser(description=L2D_IsingDynamicsSerializer_description, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-# required arguments
-
-
-# Optional parameters
-for ov in parserDict_Opt.keys():
-    parser.add_argument(*parsDict_get(parserDict_Opt, ov, 'names'),
-        default=parsDict_get(parserDict_Opt, ov, 'default'),
-        help=parsDict_get(parserDict_Opt, ov, 'help'),
-        type=parsDict_get(parserDict_Opt, ov, 'type'),
-        nargs=parsDict_get(parserDict_Opt, ov, 'nargs')
-    )
-# Optional parameters
-def parsDict_get(var, key):
-    return parDA[var].get(key, None)
-for ov in parDA.keys():
-    parser.add_argument(*parsDict_get(ov, 'names'),
-        default=parsDict_get(ov, 'default'),
-        help=parsDict_get(ov, 'help'),
-        action=parsDict_get(ov, 'action')
-    )
+parser = argparse.ArgumentParser(
+    description=L2D_IsingDynamicsSerializer_description, 
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter
+)
+#
+for k,v in {**optional_args_dict, **action_args_dict}.items():
+    parser.add_argument(*k, **v)
