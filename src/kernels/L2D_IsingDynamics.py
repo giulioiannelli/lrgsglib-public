4d695b5f90ae55c5dd103196896e369665697009
giulioiannelli <giulioiannelli.w@gmail.com>
2025-03-07 18:48:31 +0100

    Refactor run_simulation in L2D_IsingDynamics; simplify argument handling and improve clarity by removing unnecessary parameters and enhancing function calls.

--- src/kernels/L2D_IsingDynamics.py
+++ src/kernels/L2D_IsingDynamics.py
@@ -12,8 +12,9 @@ def initialize_ising_dict_args(args, out_suffix, NoClust):
 def get_out_suffix(args):
     return join_non_empty('_', args.init_cond, args.cell_type, args.out_suffix)
 
-
-def run_simulation(args, number, remove_files, out_suffix):
+def run_simulation(args):
+    ic_gs = args.init_cond.startswith('ground_state') or args.init_cond.startswith('gs')
+    number = int(args.init_cond.split('_')[-1]) if ic_gs else 0
     for _ in range(args.number_of_averages):
         lattice = Lattice2D(**initialize_l2d_dict_args(args))
         lattice.flip_sel_edges(lattice.nwDict[args.cell_type]['G'])
@@ -22,7 +23,7 @@ def run_simulation(args, number, remove_files, out_suffix):
         lattice.make_clustersYN(f'eigV{number}', val=args.val)
         #
         NoClust = lattice.handle_no_clust(NoClust=args.NoClust)
-        isingDictArgs = initialize_ising_dict_args(args, out_suffix, NoClust)
+        isingDictArgs = initialize_ising_dict_args(args, get_out_suffix(args), NoClust)
         #
         if NoClust is None:
             continue
@@ -34,8 +35,8 @@ def run_simulation(args, number, remove_files, out_suffix):
                 lattice.export_ising_clust(exName=isdy.id_string_isingdyn, 
                                         NoClust=NoClust)
                 if args.runlang == 'C4':
-                    lattice.export_eigV(number, exName=isdy.id_string_isingdyn, print_msg=args.verbose)
+                    lattice.export_eigV(number, exName=isdy.id_string_isingdyn, verbose=args.verbose)
         isdy.run(verbose=args.verbose)
-        if remove_files:
+        if args.remove_files:
             isdy.remove_run_c_files(remove_stderr=True)
             lattice.remove_exported_files()
