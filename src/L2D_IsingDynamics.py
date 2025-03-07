a14e5c9b07eecb004de2437f6c3341c115fadf3f
giulioiannelli <giulioiannelli.w@gmail.com>
2025-03-07 18:48:18 +0100

    Refactor L2D_IsingDynamics and L2D_IsingDynamics_Serialiser to simplify argument handling; streamline main function and improve code clarity.

--- src/L2D_IsingDynamics.py
+++ src/L2D_IsingDynamics.py
@@ -3,10 +3,7 @@ from kernels.L2D_IsingDynamics import *
 
 def main():
     args = parse_arguments(parser)
-    ic_gs = args.init_cond.startswith('ground_state') or args.init_cond.startswith('gs')
-    number = int(args.init_cond.split('_')[-1]) if ic_gs else 0
-    out_suffix = get_out_suffix(args)
-    run_simulation(args, number, args.remove_files, out_suffix)
+    run_simulation(args)
     #
     if args.print_chrono:
         Chronometer.print_all_chronometers()
--- src/L2D_IsingDynamics_Serialiser.py
+++ src/L2D_IsingDynamics_Serialiser.py
@@ -3,91 +3,58 @@ from lrgsglib.config.funcs import *
 from lrgsglib.config.progargs import *
 from lrgsglib.shared import *
 #
-args = parse_arguments(parser)
-#
-geo = args.geometry
-cell = args.cell_type
-navg = args.number_of_averages
-ic = args.init_cond
-navg = args.number_of_averages
-out_suffix = args.out_suffix
-in_suffix = args.in_suffix
-NoClust = args.NoClust
-runlang = args.runlang
-thrmsteps = args.thermsteps
-ic_gs = args.init_cond.startswith('ground_state')
-number = int(args.init_cond.split('_')[-1]) if ic_gs else 0
-workdir = args.workdir
-#
-progn = L2D_IsingDynamics_progName
-progn_shrt = L2D_IsingDynamics_progNameShrt
-exec_bool = args.exec
-prnt_bool = args.print
-#
-side_list = args.side1_list
-navg_list = [navg for _ in range(len(side_list))]
-pflp_list = args.pflip_linsp
-temp_list = args.Temp_linsp
+def main():
+    args, unknown = parser.parse_known_args()
+    progn = L2D_IsingDynamics_progName
+    progn_shrt = L2D_IsingDynamics_progNameShrt
+    exec_bool, prnt_bool = args.exec, args.print
+    side_list = args.side1_list
+    pflp_list = args.pflip_linsp
+    temp_list = args.Temp_linsp
 
-if args.slanzarv_minMB == args.slanzarv_maxMB:
-    def memoryfunc(*_):
-        return args.slanzarv_minMB
-else:
-    def memoryfunc(x):
-        hl_side = [min(side_list), max(side_list)]
-        hl_memy = [args.slanzarv_minMB, args.slanzarv_maxMB]
-        return np.interp(x, hl_side, hl_memy).astype(int)
-def slanzarv_str(mode, L, p, geo, c, T, 
-                 nomail=True, moretime=False, short=False):
-    jobname = join_non_empty('_', progn_shrt, out_suffix,
-                            f"{L}", f"{p:.3g}", f"{T:.3g}", geo[:3], c)
-    slanzarvopt = f"--jobname {jobname} "
-    if nomail:
-        slanzarvopt += "--nomail "
-    if moretime:
-        slanzarvopt += " --time " + moretime + ""
-    if short:
-        slanzarvopt += "--short"
-    slanzarvstr = f"slanzarv -m {memoryfunc(L)} {slanzarvopt}"
-    return slanzarvstr
-#
-if exec_bool or prnt_bool:
-    count = 0
-    count_exe = 0
-    def operate(s):
-        global count, count_exe
-        if exec_bool and prnt_bool:
-            print(s)
-            os.system(s)
-            count += 1
-            count_exe += 1
-        elif exec_bool:
-            os.system(s)
-            count_exe += 1
-        elif prnt_bool:
-            print(s)
-            count += 1
-    def exec_str(L, p, geo, cell, navg, T, runlang, in_suffix, out_suffix, ic,
-                 NoClust, workdir=workdir):
-        lnchStr = f"python src/{progn}.py"
-        instr = ' '.join([f"-is ", f"{in_suffix}" or f"p={p:.3g}"])
-        outstr = f"-os {out_suffix} " if out_suffix else ""
-        wdstr = f"-wd {workdir} " if workdir else ""
-        argstr = join_non_empty(' ', f"{L}", f"{p:.3g}", f"{T:.3g}", 
-                                f"-g {geo}", f"-c {cell}", f"-n {navg}", 
-                                f"-ic {ic}", f"-rl {runlang}", instr,
-                                outstr, f"-nc {NoClust}", f"-ts {thrmsteps}", wdstr)
-        slanzarvStr = slanzarv_str(runlang, L, p, geo, cell, T)
-        return ' '.join([slanzarvStr, lnchStr, argstr])
+    # Memory function definition
+    if args.slanzarv_minMB == args.slanzarv_maxMB:
+        memoryfunc = lambda *_: args.slanzarv_minMB
+    else:
+        def memoryfunc(x):
+            hl_side = [min(side_list), max(side_list)]
+            hl_memy = [args.slanzarv_minMB, args.slanzarv_maxMB]
+            return np.interp(x, hl_side, hl_memy).astype(int)
+    # Operate if either print or execute is requested
+    if exec_bool or prnt_bool:
+        count = count_exe = 0
 
-else:
-    exit(0)
+        def operate(L, p, T):
+            nonlocal count, count_exe
+            progargs = [str(L), f"{p:.3g}", f"{T:.3g}"]
+            opts = []
+            opts.append("-m")
+            opts.append(f"{memoryfunc(L)}")
+            if args.nomail:
+                opts.append("--nomail")
+            if args.short:
+                opts.append("--short")
+            if args.moretime:
+                opts.append(f"--time {args.moretime}")
+            opts.append("--jobname")
+            opts.append(f"{join_non_empty('_', progn_shrt, args.slanzarv_id, *progargs)}")
+            cmd = ['python', f'{progn}.py'] + progargs + unknown
+            slanz_cmd = ["slanzarv"] + opts + cmd
+            #
+            if prnt_bool:
+                print(' '.join(slanz_cmd))
+                count += 1
+            if exec_bool:
+                subprocess.run(slanz_cmd)
+                count_exe += 1
 
-for L,navv in zip(side_list, navg_list):
-    for p in pflp_list:
-            for T in temp_list:
-                estring = exec_str(L, p, geo, cell, navv, T, 
-                                   runlang, in_suffix, out_suffix, ic, NoClust)
-                operate(estring)
-print(f"Total number of jobs executed: {count_exe}")
-print(f"Total number of jobs printed: {count}")
+        for L in side_list:
+            for p in pflp_list:
+                for T in temp_list:
+                    operate(L, p, T)
+
+        print(f"Total number of jobs executed: {count_exe}")
+        print(f"Total number of jobs printed: {count}")
+
+if __name__ == '__main__':
+    main()
