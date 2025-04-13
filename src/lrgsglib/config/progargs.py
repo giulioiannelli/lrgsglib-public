b00da2ee52aa280d5345d8b952ae6abfaec45fe7
giulioiannelli <giulioiannelli.w@gmail.com>
2025-04-13 16:21:54 +0200

    Refactor program arguments structure and add action argument dictionaries for improved command-line interface

--- src/lrgsglib/config/progargs.py
+++ src/lrgsglib/config/progargs.py
@@ -5,7 +5,7 @@ import numpy as np
 phelp_exc = "Option to execute the output of the Serialiser"
 phelp_freq = "Frequency of the saved spin output"
 phelp_insuffix = "Suffix for input files"
-phelp_L = "Size of the square lattice"
+phelp_L = "Main side of the lattice"
 phelp_mMB = "Minimum MB quantity to be allocated for the single process"
 phelp_MMB = "Maximum MB quantity to be allocated for the single process"
 phelp_moretime = "Time allocation for the slanzarv job"
@@ -52,49 +52,8 @@ DEFAULT_CELL = 'rand'
 DEFAULT_NOCLUST = 1
 DEFAULT_PFLIP_LINSP = np.linspace(0.01, 0.5, 10)
 DEFAULT_TEMP_LINSP = np.linspace(0.1, 2.2, 10)
-# Lattices program arguments
-## program helpers
-phelp_geo = "Geometry of the lattice"
-phelp_side1_list = "List of side1 values for L2D Serializers"
-## default values
-DEFAULT_GEO = 'squared'
-DEFAULT_SIDE1_LIST = [16, 32, 64]
-# IsingDynamics program arguments
-## program helpers
-phelp_ic = "Initial condition for the Ising model"
-phelp_runlang = "Language for running the Ising model"
-phelp_thrmsteps = "Number of thermalization steps"
-## default values
-DEFAULT_INIT_COND = 'ground_state_0'
-DEFAULT_RUNLANG = 'C1'
-DEFAULT_THRMSTEPS = 20
-
-## names and descriptions
-L2D_IsingDynamics_progName = 'L2D_IsingDynamics'
-L2D_IsingDynamics_progNameShrt = 'L2DID'
-L2D_IsingDynamics_description = f"""
-    Computational resourses regarding the Ising Dynamics of 2D 
-    lattices: {L2D_IsingDynamics_progName}.py
-"""
-L2D_IsingDynamicsSerializer_description = f"""
-    Serialiser for {L2D_IsingDynamics_progName}.py
-"""
-## arg parsers dict
-L2D_IsingDynamics_args = {
-    'L': {
-        'help': phelp_L, 
-        'type': int
-    },
-    'p': {
-        'help': phelp_p,
-        'type': float
-    },
-    'T': {
-        'help': phelp_T,
-        'type': float
-    }
-}
-L2D_IsingDynamics_action_args_dict = {
+# Program arguments
+action_args_dict = {
     tuple(['-rf', '--remove_files']): {
         'help': phelp_remove_files,
         'action': argparse.BooleanOptionalAction,
@@ -108,21 +67,103 @@ L2D_IsingDynamics_action_args_dict = {
         'action': argparse.BooleanOptionalAction,
         'default': DEFAULT_PRINT_CHRONO}
 }
-L2D_IsingDynamics_optional_args_dict = {
+# Serializers program arguments
+Serializer_action_args_dict = {
+    tuple(['--exec']): {
+        'help': phelp_exc,
+        'action': argparse.BooleanOptionalAction,
+        'default': DEFAULT_EXEC
+    },
+    tuple(['--print']): {
+        'help': phelp_print,
+        'action': argparse.BooleanOptionalAction,
+        'default': DEFAULT_PRINT
+    },
+    tuple(['--nomail']): {
+        'help': phelp_nomail,
+        'action': argparse.BooleanOptionalAction,
+        'default': DEFAULT_NOMAIL
+    },
+    tuple(['--short']): {
+        'help': phelp_short,
+        'action': argparse.BooleanOptionalAction,
+        'default': DEFAULT_SHORT
+    },
+}
+# Lattices program arguments
+## program helpers
+phelp_geo = "Geometry of the lattice"
+phelp_side1_list = "List of side1 values for L2D Serializers"
+## default values
+DEFAULT_GEO = 'squared'
+DEFAULT_SIDE1_LIST = [16, 32, 64]
+## args parsers dict
+L2D_args = {
+    'L': {
+        'help': phelp_L, 
+        'type': int
+    },
+    'p': {
+        'help': phelp_p,
+        'type': float
+    }
+}
+L2D_opt_args = {
     tuple(['-c', '--cell_type']): {
         'help': phelp_cell,
         'type': str,
         'default': DEFAULT_CELL
     },
-    tuple(['-fq', '--freq']): {
-        'help': phelp_freq,
-        'type': int,
-        'default': DEFAULT_FREQ
-    },
     tuple(['-g', '--geometry']): {
         'help': phelp_geo,
         'type': str,
         'default': DEFAULT_GEO
+    },
+    tuple(['-na', '--number_of_averages']): {
+        'help': phelp_navg,
+        'type': int,
+        'default': DEFAULT_NAVG
+    },
+    tuple(['-wd', '--workdir']): {
+        'help': phelp_workdir,
+        'type': str,
+        'default': DEFAULT_WORKDIR
+    }
+}
+# IsingDynamics program arguments
+## program helpers
+phelp_ic = "Initial condition for the Ising model"
+phelp_runlang = "Language for running the Ising model"
+phelp_thrmsteps = "Number of thermalization steps"
+## default values
+DEFAULT_INIT_COND = 'ground_state_0'
+DEFAULT_RUNLANG = 'C1'
+DEFAULT_THRMSTEPS = 20
+## names and descriptions
+L2D_IsingDynamics_progName = 'L2D_IsingDynamics'
+L2D_IsingDynamics_progNameShrt = 'L2DID'
+L2D_IsingDynamics_description = f"""
+    Computational resourses regarding the Ising Dynamics of 2D 
+    lattices: {L2D_IsingDynamics_progName}.py
+"""
+L2D_IsingDynamicsSerializer_description = f"""
+    Serialiser for {L2D_IsingDynamics_progName}.py
+"""
+## arg parsers dict
+L2D_IsingDynamics_args = {
+    **L2D_args, 
+    'T': {
+        'help': phelp_T,
+        'type': float
+    }
+}
+L2D_IsingDynamics_action_args_dict = {**action_args_dict}
+L2D_IsingDynamics_optional_args_dict = {
+    **L2D_opt_args,
+    tuple(['-fq', '--freq']): {
+        'help': phelp_freq,
+        'type': int,
+        'default': DEFAULT_FREQ
     },
     tuple(['-ic', '--init_cond']): {
         'help': phelp_ic,
@@ -133,11 +174,6 @@ L2D_IsingDynamics_optional_args_dict = {
         'help': phelp_insuffix,
         'type': str,
         'default': DEFAULT_INSFFX
-    },
-    tuple(['-na', '--number_of_averages']): {
-        'help': phelp_navg,
-        'type': int,
-        'default': DEFAULT_NAVG
     },
     tuple(['-nc', '--NoClust']): {
         'help': phelp_NoClust,
@@ -163,11 +199,6 @@ L2D_IsingDynamics_optional_args_dict = {
         'help': phelp_val,
         'type': str,
         'default': DEFAULT_VAL
-    },
-    tuple(['-wd', '--workdir']): {
-        'help': phelp_workdir,
-        'type': str,
-        'default': DEFAULT_WORKDIR
     }
 }
 L2D_IsingDynamicsSerializer_optional_args_dict = {
@@ -213,25 +244,24 @@ L2D_IsingDynamicsSerializer_optional_args_dict = {
         'default': DEFAULT_SLANZARV_ID
     },
 }
-L2D_IsingDynamicsSerializer_action_args_dict = {
-    tuple(['--exec']): {
-        'help': phelp_exc,
-        'action': argparse.BooleanOptionalAction,
-        'default': DEFAULT_EXEC
-    },
-    tuple(['--print']): {
-        'help': phelp_print,
-        'action': argparse.BooleanOptionalAction,
-        'default': DEFAULT_PRINT
-    },
-    tuple(['--nomail']): {
-        'help': phelp_nomail,
-        'action': argparse.BooleanOptionalAction,
-        'default': DEFAULT_NOMAIL
-    },
-    tuple(['--short']): {
-        'help': phelp_short,
-        'action': argparse.BooleanOptionalAction,
-        'default': DEFAULT_SHORT
-    },
+L2D_IsingDynamicsSerializer_action_args_dict = {**Serializer_action_args_dict}
+# Signed Laplacian Spectra program arguments
+## program helpers
+phelp_ic = "Initial condition for the Ising model"
+## default values
+DEFAULT_INIT_COND = 'ground_state_0'
+## names and descriptions
+L2D_SlaplSpect_progName = 'L2D_SlaplSpect'
+L2D_SlaplSpect_progNameShrt = 'L2DSS'
+L2D_SlaplSpect_description = f"""
+    Computational resourses regarding the Signed Laplacian spectrum of 2D 
+    lattices: {L2D_IsingDynamics_progName}.py
+"""
+## arg parsers dict
+L2D_IsingDynamics_args = {
+    **L2D_args, 
+    'T': {
+        'help': phelp_T,
+        'type': float
+    }
 }
